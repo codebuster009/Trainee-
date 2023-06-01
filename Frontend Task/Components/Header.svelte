bde476606c238f7b896e3cd40877f54d7f1bcd63
Kartavya Sharma <122731680+codebuster009@users.noreply.github.com>
2023-06-01 19:26:59 +0530

    Update Header.svelte

--- Frontend Task/Components/Header.svelte
+++ Frontend Task/Components/Header.svelte
@@ -1,1 +1,33 @@
+<script>
+const userName = JSON.parse(localStorage.getItem("register"))
+import {useNavigate } from "svelte-navigator";
+const navigate = useNavigate();
 
+
+function handleLogout(){
+localStorage.removeItem("register")
+navigate("/login")
+}
+</script>
+
+<main>
+<div class="header"></div>
+{#if userName}
+<h1 class = "user">{userName.userName}</h1>
+{:else}
+<h1>Username</h1>
+{/if}
+<button type ="button" on:click={handleLogout}>Logout</button>
+</main>
+
+<style>
+.user{
+    color: rgb(255, 234, 234);
+    margin-left: 660px;
+}
+button{
+    margin-left: 750px;
+    background: linear-gradient(to right top, #672280, #105685);
+  color: white;
+}
+</style>
