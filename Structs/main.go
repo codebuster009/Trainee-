cd8c00d008b8782ff8d85e0b13ce9c426a2a1ff8
Kartavya Sharma <122731680+codebuster009@users.noreply.github.com>
2023-06-02 17:44:26 +0530

    Create main.go

---
+++ Structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	ZipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			ZipCode: 94000,
+		},
+	}
+	//shortcut
+	jim.updateName("jimmy") //no pointer
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
